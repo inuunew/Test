2d8c1d0455b409be8a52d297e110656cc99b28d1
inuunew <choirulazzamibnu@gmail.com>
2026-05-09 21:46:09 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'inuutyz-music-v1';
+const urlsToCache = [
+    '/'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch & Intercept (Network First untuk API agar lagu selalu fresh)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
