a40ef67c4545cdaa9a3890030f2a1456bfa0ebf2
inuunew <choirulazzamibnu@gmail.com>
2026-08-23 11:33:22 +0700

    Create feed.js

---
+++ api/feed.js
@@ -0,0 +1,12 @@
+const { VeloflixEngine, cleanJson } = require('../lib/engine');
+
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    try {
+        const engine = new VeloflixEngine();
+        const feed = await engine.getHomepageFullFeed();
+        res.status(200).json(cleanJson({ status: 200, creator: 'Lann', ...feed }));
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
